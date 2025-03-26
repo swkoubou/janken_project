d04480ebc9c47f9be357ce00eb466567a44276e2
Ryuuichi <131429001+HagaRyuiti@users.noreply.github.com>
2025-03-27 00:32:03 +0900

    dockerテスト

--- create_dataset/app.py
+++ create_dataset/app.py
@@ -95,4 +95,4 @@ def get_landmarks():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(debug=True, threaded=True, host='0.0.0.0')
